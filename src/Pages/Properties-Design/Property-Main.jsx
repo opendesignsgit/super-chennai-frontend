ff28fdc55852547afa231520f686e3f9bc9d6bd5
DhineshODI <dhineshkodi2024@gmail.com>
2025-09-25 17:59:05 +0530

    Today-Update

--- src/Pages/Properties-Design/Property-Main.jsx
+++ src/Pages/Properties-Design/Property-Main.jsx
@@ -24,8 +24,8 @@ export default function PropertyMainPage() {
 
       {/* Filter Section */}
 
-      <div>
-        <div className="container max-w-7xl mx-auto px-4">
+      <div className="bg-[#f4f5f7]">
+        <div className="container max-w-7xl mx-auto px-4 !mt-0 !mb-0  mainConiatinerPropertyList">
           <div className="sidebar-stylescss">
             {/* Side-Bar */}
 
@@ -59,17 +59,281 @@ export default function PropertyMainPage() {
               </div>
 
               <div className="PropertiesCards">
-                <div>
-                  <div>
+                <div className="PropertiesCardsSection">
+                  <div className="mainPropertiesCards">
                     <img
+                      className="propertyImage"
                       src="https://imagecdn.99acres.com/media1/32526/12/650532437M-1758453715074.jpg"
                       alt=""
                     />
 
-                    <div>Shivananda Flats</div>
-                    <div>3 BHK Flat for rent in Zamin Pallavaram, Chennai</div>
+                    <div className="propertyDetails">
+                      <h4>Shivananda Flats</h4>
+                      <h5>3 BHK Flat for rent in Zamin Pallavaram, Chennai</h5>
 
-                    <div></div>
+                      <div className="aboutPlotsSize">
+                        <div className="flex flex-col items-start">
+                          <span>₹86.4 Lac</span>
+                          <span>₹3,600 /sqft</span>
+                        </div>
+                        <div className="flex flex-col items-start">
+                          <span>2,400 sqft</span>
+                          <span>Plot Area</span>
+                        </div>
+                        <div className="flex flex-col items-start">
+                          <span>Plot/Land</span>
+                          <span>Ready To Move</span>
+                        </div>
+                      </div>
+
+                      <p className="propertContent">
+                        Arunachala mountain view to the north, open park/garden
+                        on the west, corner plot for privacy, 23 ft road, 1.7 km
+                        <br />
+                      </p>
+                      <div className=" cursor-pointer flex items-center">
+                        <a href="" className="propertyViewDetails">
+                          View Details
+                        </a>
+                        <img
+                          className="propertyAroWwCLick"
+                          src="/images/icons/right-side-arrow-superchennai.svg"
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="uploadedDetailsproperty">
+                        <div className="flex flex-col">
+                          <span className="monthsGaoProperty">2mo ago</span>{" "}
+                          <span className="ownerproperty">Owner</span>
+                        </div>
+
+                        <div className="propertyViewButton">
+                          <button>View Button</button>
+                          <button>Contact</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mainPropertiesCards">
+                    <img
+                      className="propertyImage"
+                      src="https://imagecdn.99acres.com/media1/32526/12/650532437M-1758453715074.jpg"
+                      alt=""
+                    />
+
+                    <div className="propertyDetails">
+                      <h4>Shivananda Flats</h4>
+                      <h5>3 BHK Flat for rent in Zamin Pallavaram, Chennai</h5>
+
+                      <div className="aboutPlotsSize">
+                        <div className="flex flex-col items-start">
+                          <span>₹86.4 Lac</span>
+                          <span>₹3,600 /sqft</span>
+                        </div>
+                        <div className="flex flex-col items-start">
+                          <span>2,400 sqft</span>
+                          <span>Plot Area</span>
+                        </div>
+                        <div className="flex flex-col items-start">
+                          <span>Plot/Land</span>
+                          <span>Ready To Move</span>
+                        </div>
+                      </div>
+
+                      <p className="propertContent">
+                        Arunachala mountain view to the north, open park/garden
+                        on the west, corner plot for privacy, 23 ft road, 1.7 km
+                        <br />
+                      </p>
+                      <div className=" cursor-pointer flex items-center">
+                        <a href="" className="propertyViewDetails">
+                          View Details
+                        </a>
+                        <img
+                          className="propertyAroWwCLick"
+                          src="/images/icons/right-side-arrow-superchennai.svg"
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="uploadedDetailsproperty">
+                        <div className="flex flex-col">
+                          <span className="monthsGaoProperty">2mo ago</span>{" "}
+                          <span className="ownerproperty">Owner</span>
+                        </div>
+
+                        <div className="propertyViewButton">
+                          <button>View Button</button>
+                          <button>Contact</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mainPropertiesCards">
+                    <img
+                      className="propertyImage"
+                      src="https://imagecdn.99acres.com/media1/32526/12/650532437M-1758453715074.jpg"
+                      alt=""
+                    />
+
+                    <div className="propertyDetails">
+                      <h4>Shivananda Flats</h4>
+                      <h5>3 BHK Flat for rent in Zamin Pallavaram, Chennai</h5>
+
+                      <div className="aboutPlotsSize">
+                        <div className="flex flex-col items-start">
+                          <span>₹86.4 Lac</span>
+                          <span>₹3,600 /sqft</span>
+                        </div>
+                        <div className="flex flex-col items-start">
+                          <span>2,400 sqft</span>
+                          <span>Plot Area</span>
+                        </div>
+                        <div className="flex flex-col items-start">
+                          <span>Plot/Land</span>
+                          <span>Ready To Move</span>
+                        </div>
+                      </div>
+
+                      <p className="propertContent">
+                        Arunachala mountain view to the north, open park/garden
+                        on the west, corner plot for privacy, 23 ft road, 1.7 km
+                        <br />
+                      </p>
+                      <div className=" cursor-pointer flex items-center">
+                        <a href="" className="propertyViewDetails">
+                          View Details
+                        </a>
+                        <img
+                          className="propertyAroWwCLick"
+                          src="/images/icons/right-side-arrow-superchennai.svg"
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="uploadedDetailsproperty">
+                        <div className="flex flex-col">
+                          <span className="monthsGaoProperty">2mo ago</span>{" "}
+                          <span className="ownerproperty">Owner</span>
+                        </div>
+
+                        <div className="propertyViewButton">
+                          <button>View Button</button>
+                          <button>Contact</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mainPropertiesCards">
+                    <img
+                      className="propertyImage"
+                      src="https://imagecdn.99acres.com/media1/32526/12/650532437M-1758453715074.jpg"
+                      alt=""
+                    />
+
+                    <div className="propertyDetails">
+                      <h4>Shivananda Flats</h4>
+                      <h5>3 BHK Flat for rent in Zamin Pallavaram, Chennai</h5>
+
+                      <div className="aboutPlotsSize">
+                        <div className="flex flex-col items-start">
+                          <span>₹86.4 Lac</span>
+                          <span>₹3,600 /sqft</span>
+                        </div>
+                        <div className="flex flex-col items-start">
+                          <span>2,400 sqft</span>
+                          <span>Plot Area</span>
+                        </div>
+                        <div className="flex flex-col items-start">
+                          <span>Plot/Land</span>
+                          <span>Ready To Move</span>
+                        </div>
+                      </div>
+
+                      <p className="propertContent">
+                        Arunachala mountain view to the north, open park/garden
+                        on the west, corner plot for privacy, 23 ft road, 1.7 km
+                        <br />
+                      </p>
+                      <div className=" cursor-pointer flex items-center">
+                        <a href="" className="propertyViewDetails">
+                          View Details
+                        </a>
+                        <img
+                          className="propertyAroWwCLick"
+                          src="/images/icons/right-side-arrow-superchennai.svg"
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="uploadedDetailsproperty">
+                        <div className="flex flex-col">
+                          <span className="monthsGaoProperty">2mo ago</span>{" "}
+                          <span className="ownerproperty">Owner</span>
+                        </div>
+
+                        <div className="propertyViewButton">
+                          <button>View Button</button>
+                          <button>Contact</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mainPropertiesCards">
+                    <img
+                      className="propertyImage"
+                      src="https://imagecdn.99acres.com/media1/32526/12/650532437M-1758453715074.jpg"
+                      alt=""
+                    />
+
+                    <div className="propertyDetails">
+                      <h4>Shivananda Flats</h4>
+                      <h5>3 BHK Flat for rent in Zamin Pallavaram, Chennai</h5>
+
+                      <div className="aboutPlotsSize">
+                        <div className="flex flex-col items-start">
+                          <span>₹86.4 Lac</span>
+                          <span>₹3,600 /sqft</span>
+                        </div>
+                        <div className="flex flex-col items-start">
+                          <span>2,400 sqft</span>
+                          <span>Plot Area</span>
+                        </div>
+                        <div className="flex flex-col items-start">
+                          <span>Plot/Land</span>
+                          <span>Ready To Move</span>
+                        </div>
+                      </div>
+
+                      <p className="propertContent">
+                        Arunachala mountain view to the north, open park/garden
+                        on the west, corner plot for privacy, 23 ft road, 1.7 km
+                        <br />
+                      </p>
+                      <div className=" cursor-pointer flex items-center">
+                        <a href="" className="propertyViewDetails">
+                          View Details
+                        </a>
+                        <img
+                          className="propertyAroWwCLick"
+                          src="/images/icons/right-side-arrow-superchennai.svg"
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="uploadedDetailsproperty">
+                        <div className="flex flex-col">
+                          <span className="monthsGaoProperty">2mo ago</span>{" "}
+                          <span className="ownerproperty">Owner</span>
+                        </div>
+
+                        <div className="propertyViewButton">
+                          <button>View Button</button>
+                          <button>Contact</button>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>

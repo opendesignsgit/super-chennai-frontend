89ea1a98004ec2f550e7d2e6a6d02b6600ba9e57
Bharathi <bharathi0307@gmail.com>
2025-12-09 18:59:12 +0530

    remove api

--- config.jsx
+++ config.jsx
@@ -6,6 +6,6 @@
 
 // export const API_BASE_URL_API = "http://localhost:3000/api"
 export const API_BASE_URL_API = "https://api.superchennai.com/api"
-export const API_BASE_URL = "https://cms.superchennai.com"; 
+export const API_BASE_URL = "https://demo.superchennai.com"; 
 // export const API_BASE_URL = "http://localhost:3000";  
 

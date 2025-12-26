39c75e48c67fb95687b745f30eb723b8b64bf23c
Bharathi <bharathi0307@gmail.com>
2025-12-26 15:51:53 +0530

    bug fix

--- src/Pages/Activities/MargazhiMonth/MargazhiPage.jsx
+++ src/Pages/Activities/MargazhiMonth/MargazhiPage.jsx
@@ -596,9 +596,36 @@ const [selectedItem, setSelectedItem] = useState(null);
                             target="_blank"
                             rel="noopener noreferrer"
                             onClick={(e) => e.stopPropagation()}
-                            className="text-xs font-medium text-[#a44294] underline truncate max-w-[70%]"
+                            className="flex items-center gap-1 text-xs underline truncate max-w-[70%] text-[#a44294]"
                           >
-                            📍 {item.place.name}
+                            {/* Location SVG */}
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="w-4 h-4 flex-shrink-0 text-[#a44294]"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                              strokeWidth={2}
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M12 10.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z"
+                              />
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M19.5 10.5c0 6-7.5 10.5-7.5 10.5S4.5 16.5 4.5 10.5a7.5 7.5 0 1115 0z"
+                              />
+                            </svg>
+
+                            <span className="font-semibold text-gray-700">
+                              Location:
+                            </span>
+
+                            <span className="font-medium truncate">
+                              {item.place.name}
+                            </span>
                           </a>
                         ) : (
                           <span />

505d4b4b8c5e26080597a26605000c55119be446
DhineshODI <dhineshkodi2024@gmail.com>
2026-04-30 11:38:59 +0530

    sticky-added-style

--- src/Pages/ChennaiNeighbourhoods/Components/Search.jsx
+++ src/Pages/ChennaiNeighbourhoods/Components/Search.jsx
@@ -1,41 +1,67 @@
-
-
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react";
 
 export default function SearchChennai({ onSearch }) {
-  const [value, setValue] = useState("")
+  const [value, setValue] = useState("");
+  const [isTop, setIsTop] = useState(false);
+
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const getHeaderHeight = () => {
+      const width = window.innerWidth;
+
+      if (width >= 1024) return 380;
+      if (width >= 768) return 280;
+      return 100;
+    };
+
+    const handleScroll = () => {
+      if (!sectionRef.current) return;
+
+      const sectionOffsetTop = sectionRef.current.offsetTop;
+      const headerHeight = getHeaderHeight();
+      const scrollY = window.scrollY;
+
+      if (scrollY + headerHeight >= sectionOffsetTop) {
+        setIsTop(true);
+      } else {
+        setIsTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    handleScroll();
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   const handleSearch = () => {
-    onSearch(value)
-  }
+    onSearch(value);
+  };
 
   return (
-
-    <div className="container max-w-7xl mx-auto px-4 seachconatiners"> 
-      <div className="mb-6 filtersearchnewmain">
-        <div className="flex gap-2 filtersearchnew">
-
+    <div
+      className={`container max-w-7xl mx-auto px-4 seachconatiners ${isTop ? "neighstickkkk" : "noneighstickkkk"}`}
+    >
+      <div className="mb-6 filtersearchnewmain" ref={sectionRef}>
+        <div className="flex gap-2 filtersearchnew" >
           <input
             className="border p-3 w-full rounded-lg searchhoodfilter"
             placeholder="Find your Location / PIN code"
             value={value}
-            onChange={(e)=>setValue(e.target.value)}
-            onKeyDown={(e)=>{
-              if(e.key === "Enter"){
-                handleSearch()
+            onChange={(e) => setValue(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSearch();
               }
             }}
           />
 
-          <button
-            onClick={handleSearch}
-            className="searchbuttonfilt"
-          >
+          <button onClick={handleSearch} className="searchbuttonfilt">
             Search
           </button>
-
         </div>
       </div>
     </div>
-  )
+  );
 }

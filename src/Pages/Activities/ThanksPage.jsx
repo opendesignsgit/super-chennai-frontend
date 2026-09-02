289133146f8e904699ea7fbf7bcee3d628c9ff6c
Bharathi <bharathi0307@gmail.com>
2026-09-02 10:57:08 +0530

    bug fix

--- src/Pages/Activities/ThanksPage.jsx
+++ src/Pages/Activities/ThanksPage.jsx
@@ -5,8 +5,8 @@ import { useLocation } from "react-router-dom";
 
 export default function Thankyou() {
   const location = useLocation();
-  // const from = location.state?.from;
-  const from = "arun-jain";
+  const from = location.state?.from;
+  // const from = "arun-jain";
 
   const [imageSrc, setImageSrc] = useState("/images/thank-you-banner.jpg");
 

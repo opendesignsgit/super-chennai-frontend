b48d41f613efcedf4c72784056ed646a1e207730
Bharathi <bharathi0307@gmail.com>
2026-09-18 11:01:13 +0530

    new chabges

--- config.jsx
+++ config.jsx
@@ -21,14 +21,14 @@
 // export const API_BASE_URL_API = "http://localhost:3000/api"
 // export const API_BASE_URL = "http://localhost:3000"; 
 // export const API_BASE_URL = "https://dev-cms.superchennai.com/"; 
+// export const API_BASE_URL_API_TEST_DEV = "https://dev-cms.superchennai.com";
+// export const API_BASE_URL_API_TEST_DEV = "http://localhost:3000";
 
 //########### PRODUCTION ENV #################
 
 export const API_BASE_URL_API = "https://api.superchennai.com/api";
 export const API_BASE_URL = "https://demo.superchennai.com";
 export const API_BASE_URL_API_TEST_DEV = "https://demo.superchennai.com";
-// export const API_BASE_URL_API_TEST_DEV = "https://dev-cms.superchennai.com";
-// export const API_BASE_URL_API_TEST_DEV = "http://localhost:3000";
 export const AI_BASE_URL = "https://api.superchennai.com";
 
 
